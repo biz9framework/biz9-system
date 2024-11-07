4a456e6b2ecc4342768e619e6ea9735f658c110a
speed <speed@localhost.localdomain>
2024-11-07 06:28:48 -0500

    good

--- biz9_config.js
+++ biz9_config.js
@@ -1,4 +1,4 @@
 exports.VERSION="1.0.6"
 exports.TITLE='BiZ9-System-Unstable'
-exports.REPO='git@github.com:biz9framework/biz9-system-unstable.git'
+exports.REPO='git@github.com:biz9framework/biz9-mobile-ecomapp-unstable.git'
 exports.BRANCH='main'
