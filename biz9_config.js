9e0f3736b86181833da619a87f754c68743bd846
think1 <think1@localhost.localdomain>
2024-12-03 19:19:40 -0500

    Added mobile react build.sh

--- biz9_config.js
+++ biz9_config.js
@@ -1,5 +1,5 @@
-exports.VERSION="1.2.25"
+exports.VERSION="1.2.26"
 exports.TITLE='BiZ9-System'
 exports.REPO='git@github.com:biz9framework/biz9-system.git'
-exports.BRANCH='1.2.24'
+exports.BRANCH='main'
 
