ba9237cf14dc66283905e78797c424414de49581
think1 <think1@localhost.localdomain>
2024-12-04 22:07:49 -0500

    good

--- biz9_config.js
+++ biz9_config.js
@@ -1,4 +1,4 @@
-exports.VERSION="1.3.10"
+exports.VERSION="1.3.20"
 exports.TITLE='BiZ9-System'
 exports.REPO='git@github.com:biz9framework/biz9-system.git'
 exports.BRANCH='1.2.34'
--- index.js
+++ index.js
@@ -2,15 +2,10 @@ const fs = require("fs");
 const async=require("async");
 const prompt = require('prompt-sync')();
 const { exec } = require('child_process');
-
-//const biz9_config_file="../../"+__dirname+"/biz9_config.js";
-//const package_file="../../"+__dirname+"/package.json";
 const biz9_config_file=__dirname+"/../../"+"biz9_config.js";
 const package_file=__dirname+"/../../"+"package.json";
-
 const biz9_config = require(biz9_config_file);
 const package = require(package_file);
-
 class Print {
     static show_header(title) {
         console.log('############');
